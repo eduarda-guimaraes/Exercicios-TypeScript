54789392053ea3255a87d1404c90a13b5d6b70df
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2025-07-21 16:55:40 -0300

    Primeiro commit

---
+++ nivel1/funcoesTipadas.ts
@@ -0,0 +1,13 @@
+function calcularIMC(peso: number, altura: number): number {
+  return peso / (altura * altura);
+}
+
+function classificarIMC(imc: number): string {
+  if (imc < 18.5) return "Abaixo do peso";
+  if (imc >= 18.5 && imc < 24.9) return "Normal";
+  if (imc >= 25 && imc < 29.9) return "Sobrepeso";
+  return "Obesidade";
+}
+
+const imc = calcularIMC(70, 1.75);
+console.log(classificarIMC(imc));
---
+++ nivel1/tiposBasicos.ts
@@ -0,0 +1,9 @@
+let nome: String = "Eduarda";
+let idade: Number = 18;
+let ativo: Boolean = true;
+
+//array
+let hobbies: String[] = ["dançar", "ler", "jogar"];
+
+//tupla com cordenadas
+let ponto: [number, number] = [10, 20];
